37609a07fabba5992aa911faed82a8e0c9b9be35
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-15 16:02:06 +0100

    Created App.vue

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+const baseProjectsUrl = "http://127.0.0.1:8000/api";
+import axios from "axios";
+import AppHeader from "./components/AppHeader.vue";
+export default {
+  name: "App",
+  components: { AppHeader },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  methods: {
+    fetchProjects(endpoint = null) {
+      if (!endpoint) endpoint = baseProjectsUrl + "/projects";
+      axios.get(endpoint).then((res) => {
+        this.projects = res.data;
+      });
+    },
+  },
+  created() {
+    this.fetchProjects();
+  },
+};
+</script>
+
+<template>
+  <app-header></app-header>
+  <div class="container"></div>
+</template>
+
+<style></style>
