7fe14fdd86f34c31550a7e1c9bc148fbd408ff6a
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-15 18:25:20 +0100

    Created AppAlert

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "AppAlert",
+  props: { alert: Object },
+  emits: ["close-alert"],
+};
+</script>
+
+<template>
+  <div
+    class="alert alert-dismissible fade show my-4"
+    :class="[`alert-${alert.type}`]"
+    role="alert">
+    {{ alert.message }}
+    <button
+      type="button"
+      class="btn-close"
+      @click="$emit('close-alert')"></button>
+  </div>
+</template>
+
+<style></style>
