86a7e1aa14c83eaab34439d2cebe7082bcf00108
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-15 16:34:24 +0100

    Added ProjectCard

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
 const baseProjectsUrl = "http://127.0.0.1:8000/api";
 import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
+import ProjectCard from "./components/projects/ProjectCard.vue";
 export default {
   name: "App",
-  components: { AppHeader },
+  components: { AppHeader, ProjectCard },
   data() {
     return {
       projects: [],
@@ -26,7 +27,14 @@ export default {
 
 <template>
   <app-header></app-header>
-  <div class="container"></div>
+  <div class="container">
+    <h1 class="my-5">I miei progetti</h1>
+    <ul class="row row-cols-3 list-unstyled g-3">
+      <li class="col" v-for="project in projects" :key="project.id">
+        <project-card :project="project"></project-card>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style></style>
