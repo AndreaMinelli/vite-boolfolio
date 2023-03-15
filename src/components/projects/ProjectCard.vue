a7da962da11530674443228a44eb4d1f9ad62603
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-15 16:34:03 +0100

    Created ProjectCard

---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "ProjectCard",
+  props: { project: Object },
+  computed: {
+    setDescription() {
+      let description = this.project.description;
+      if (this.project.description.length >= 47)
+        description = this.project.description.slice(0, 46) + "...";
+      return description;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="card h-100">
+    <div class="card-body">
+      <h5 class="card-title text-capitalize mb-3">{{ project.name }}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">
+        {{ project.type ? project.type.name : "N.D." }}
+      </h6>
+      <p class="card-text">
+        {{ setDescription }}
+      </p>
+    </div>
+  </div>
+</template>
