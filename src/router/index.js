443cdf164b0720f19b85272202b470b1f532e9d3
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-16 17:47:31 +0100

    Created projects-for-type route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../pages/HomePage.vue";
 import ProjectDetailsPage from "../pages/ProjectDetailsPage.vue";
+import ProjectsForTypePage from "../pages/ProjectsForTypePage.vue";
 import NotFoundPage from "../pages/NotFoundPage.vue";
 
 const router = createRouter({
@@ -16,6 +17,11 @@ const router = createRouter({
       path: "/projects/:id",
       name: "project-details",
       component: ProjectDetailsPage,
+    },
+    {
+      path: "/types/:id/projects",
+      name: "projects-for-type",
+      component: ProjectsForTypePage,
     },
     {
       path: "/:pathMatch(.*)*",
