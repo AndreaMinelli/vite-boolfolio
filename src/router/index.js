f4394273ff6f237c1f9ede298ad5972a0f475c2e
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-16 15:51:00 +0100

    Added project-details route

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../pages/HomePage.vue";
+import ProjectDetailsPage from "../pages/ProjectDetailsPage.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   linkActiveClass: "active",
-  routes: [{ path: "/", name: "home", component: HomePage }],
+  routes: [
+    { path: "/projects", name: "home", component: HomePage },
+    {
+      path: "/projects/:id",
+      name: "project-details",
+      component: ProjectDetailsPage,
+    },
+  ],
 });
 
 export { router };
